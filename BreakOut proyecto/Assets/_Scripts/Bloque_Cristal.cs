13fd870d59cdec65e9764767fb96a5b23be55dae
keeta <mario3.barcena@gmail.com>
2022-11-14 15:17:39 -0600

    Tarea M10 - Bloques BreakOut
    Se realizó la tarea solicitada para el proyecto BreakOut

--- BreakOut proyecto/Assets/_Scripts/Bloque_Cristal.cs
+++ BreakOut proyecto/Assets/_Scripts/Bloque_Cristal.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bloque_madera : Bloque
+public class Bloque_Cristal : Bloque
 {
     // Start is called before the first frame update
     void Start()
     {
-        resistencia = 3;
+        resistencia = 2; 
     }
 
     public override void RebotarBola()
